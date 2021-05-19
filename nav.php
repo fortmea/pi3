23149ba053005329a34ea1c18001cfb1d8451475
João Walter Amadeu <joaowa2002@live.com>
2021-05-18 21:09:39 -0300

    Revert "Add files via upload"
    This reverts commit 291b528f9febfbce0b7e125c59afc96016ea0ddb.

--- nav.php
+++ nav.php
@@ -1,13 +1,13 @@
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <div class="container-fluid">
-    <a class="navbar-brand" href="inicial.php" ><img class="logo-nav" src="/img/Moview_1.png "></a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php" ><img class="logo-nav" src="resources/Moview_1.png"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="inicial.php">Início</a>
+          <a class="nav-link active" aria-current="page" href="index.php">Início</a>
         </li>
 
         <li class="nav-item dropdown">
@@ -30,5 +30,5 @@
         <button class="btn btn-outline-light" type="submit">Pesquisa</button>
       </form>
     </div>
-    </div>
-  </nav>
+  </div>
+</nav>
